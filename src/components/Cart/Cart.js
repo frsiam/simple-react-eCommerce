d0f63572136861762fd30c33654f236d4e6f15f0
frsiam <siam.frs@gmail.com>
2022-03-24 10:44:19 +0600

    calculate and show the result of shipping, tax and total price

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({cart,totalPrice}) => {
+    console.log(cart)
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price
+        shipping = shipping + product.shipping
+    }
+    const tax = (total * 0.1).toFixed(2)
+
+    const grandTotal = total + shipping + (+tax)
     return (
-        <div>
+        <div className='cart'>
             <h3>Order summary</h3>
             <p>Selected Item: {cart.length}</p>
-            <h3>Total Price: ${totalPrice}</h3>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h3>Grand Total: ${grandTotal.toFixed(2)}</h3>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -6,12 +7,6 @@ import './Shop.css'
 const Shop = () => {
     const [products,setProducts] = useState([])
     const [cart, setCart] = useState([])
-    console.log(cart)
-    let totalPrice = 0
-    cart.map((s) => {
-        totalPrice = totalPrice + s.price
-        return totalPrice
-    })
 
     useEffect(()=>{
         fetch('products.json')
@@ -21,6 +16,7 @@ const Shop = () => {
     const handleAddToCart = (product) =>{
         const newCart = [...cart,product]
         setCart(newCart)
+        addToDb(product.id)
     }
     return (
         <div className='shop-container'>
@@ -30,7 +26,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <Cart cart={cart} totalPrice={totalPrice}></Cart>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
